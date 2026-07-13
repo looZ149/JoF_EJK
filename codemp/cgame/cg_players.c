2ea3f2d787b865754f2c5993e234a3eeab2273a9
Nils Klitsch <nklitsch@web.de>
2026-07-13 12:28:11 +0200

    Revert "update cg_players.c"
    This reverts commit 5a43a86840e8d30bff8e59df874f8e555648c0c0.

--- codemp/cgame/cg_players.c
+++ codemp/cgame/cg_players.c
@@ -11916,15 +11916,14 @@ skipTrail:
 		AnglesToAxis( fAng, axis );
 
 		if ( realForceLev > FORCE_LEVEL_2 )
-		{//arc°
+		{//arc
 			//trap->FX_PlayEffectID( cgs.effects.forceLightningWide, efOrg, fxDir );
 			//trap->FX_PlayEntityEffectID(cgs.effects.forceDrainWide, efOrg, axis, cent->boltInfo, cent->currentState.number, -1, -1);
-			if (cp_pluginDisable.integer & JAPRO_PLUGIN_NEWDRAINEFX) // No matter the drainfx value, we use the japro fx if plugin is enabled
+			if (cg_drainFX.integer == 2)
 				trap->FX_PlayEntityEffectID(cgs.effects.forceDrainWideJaPRO, efOrg, axis, -1, -1, -1, -1);
-			else if(cg_drainFX.integer == 1 && !(cp_pluginDisable.integer & JAPRO_PLUGIN_NEWDRAINEFX)) {
+			else if (cg_drainFX.integer == 1)
 				trap->FX_PlayEntityEffectID(cgs.effects.forceDrainWide, efOrg, axis, -1, -1, -1, -1);
-			}
-		} 
+		}
 		else
 		{//line
 			//trap->FX_PlayEffectID( cgs.effects.forceLightning, efOrg, fxDir );
